c8f4c8fa8eee83023bd103fd071a55db4179bbf1
BKerbow <bjkerbow@yahoo.com>
2021-05-17 17:00:27 -0500

    nested loop changes

--- UnitConverter/src/main/java/dev/kerbow/unitconverter/UnitConverter.java
+++ UnitConverter/src/main/java/dev/kerbow/unitconverter/UnitConverter.java
@@ -9,42 +9,64 @@ public class UnitConverter {
 		
 		while (menuSelection != 4) {
 			Scanner menuScan = new Scanner(System.in);
-			System.out.println("Please Select an Option:");
-			System.out.println("1. Cups to Teaspoons");
-			System.out.println("2. Miles to Kilometers");
-			System.out.println("3. US Gallons to Imperial Gallons");
-			System.out.println("4. Quit");
-			break;
-			
+			System.out.println("Please Select an Option to Convert:");
+			System.out.println("1. Volumes");
+			System.out.println("2. Distances");
+			System.out.println("3. Time");
+			System.out.println("4. Mass");
+			System.out.println("5. Quit");
 			
 		menuSelection = menuScan.nextInt();
 		
 			switch (menuSelection) {
 			case 1: {
-				System.out.println("Please enter the number of cups.");
-				double cups;
-				cups = scanner.nextInt();
-				System.out.println("In your provided amount of cups, there are " + (cups / 48) + " Teaspoons");
-				break;
-			}
+				System.out.println("Please Select an Option to Convert");
+				System.out.println("1. Cups to Teaspoons");
+				System.out.println("2. Liter to US Gallons");
+				System.out.println("3. US Gallon to Imperial Gallon");
+				System.out.println("4. Milimeter to US Fluid Ounce");
+				
+				menuScan.nextInt();
+				switch (menuSelection) {
+				case 1:{
+					System.out.println("Please enter the number of Cups");
+					float cups;
+					cups = menuScan.nextInt();
+					System.out.println("In your provided amount of Cups, there are " + (cups * 48) + " Teaspoons");
+					break;
+				}
+				case 2:{
+					System.out.println("Please enter the number of Liters");
+					float liters;
+					liters = menuScan.nextInt();
+					System.out.println("In your provided amount of Liters, there are " + (liters / 3.785) + " Gallons");
+					break;
+				}
+				case 3:{
+					System.out.println("Please enter the number of Gallons");
+					float gallons;
+					gallons = menuScan.nextInt();
+					System.out.println("In your provided amount of Gallons, there are " + (gallons / 1.201) + " Imperial Gallons");
+					break;
+				}
+				case 4:{
+					System.out.println("Please enter the number of Milimeters");
+					float milimeters;
+					milimeters = menuScan.nextInt();
+					System.out.println("In your provided amount of Cups, there are " + (milimeters / 29.574) + " US Fluid Ounces");
+					break;
+				}
 			case 2: {
 				System.out.println("Please enter the number of miles.");
-				//User enters Number of Miles
-				System.out.println("There are 0.6 miles in one kilometer");
+				float miles;
+				miles = menuScan.nextInt();
+				System.out.println("In your provided amount of miles, there are " + (miles * 1.609) + "Kilometers");
 				break;
 			}
 			case 3: {
-				System.out.println("Please enter the number of US Gallons.");
-				//User enters number of US gallons
-				System.out.println("There are 0.8 Gallons in one Imperial Gallon");
-				break;
-			}
-			case 4:{
 				System.out.println("You have quit the program.");
+				menuScan.close();
 				break;
-			}
-			default:{
-				System.out.println("You have selected an invalid option.");
 			}
 			}
 		}
